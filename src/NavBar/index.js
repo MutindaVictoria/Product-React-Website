e2dbe3b02d5eb72e25abfdb26f51db4b5cad8c9a
MutindaVictoria <124133802+MutindaVictoria@users.noreply.github.com>
2023-07-08 17:18:45 +0300

    Update index.js

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,29 +1,16 @@
 import React from "react";
 import './index.css';
 
-import { FaRegUserCircle } from 'react';
-
-
-const NavBar = () => {
+const Navigation = ()=>{
     return (
-      <div className="navbar">
-        <nav className="nav">
-          <ul className="ul">
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-               <a href="/">Information</a> 
-              
-            </li>
-            <li>
-                
-                <a href="/login"><FaRegUserCircle/></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+        <div>
+            <nav>
+                <ul>
+                    <li> <a href="/home">Home</a></li>
+                    <li> <a href="/products">Products</a></li>
+                </ul>
+            </nav>
+        </div>
     );
-  };
-  
-  export default NavBar;
+};
+export default Navigation
